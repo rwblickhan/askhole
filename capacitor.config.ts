fe4089f81d4fac016eeac467c44489fc6c09ee69
Russell Blickhan <rwblickhan@gmail.com>
2024-07-03 19:26:47 -0700

    Init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.rwblickhan.askhole',
+  appName: 'askhole',
+  webDir: 'dist'
+};
+
+export default config;
